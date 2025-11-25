14f14c90952e5846828a5c1d9ac96e6923e7b3d7
Ragavan <ragavanveerarajan24@gmail.com>
2025-11-25 14:31:06 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    
+    requirements = []
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+  
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    packages=find_packages(),
+    author="Ragvan Veerarajan",
+    author_email= "ragavanveerarajan24@gmail.com",
+    install_requires= get_requirements('requirements.txt')
+
+)
